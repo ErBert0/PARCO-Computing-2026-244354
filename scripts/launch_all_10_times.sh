4f44bde0cd658e4cb857afcc2fd3ebcd45c22771
Matteo Bertè <berte.mtt@gmail.com>
2025-11-17 16:53:25 +0100

    Added Another Check for the queue

--- scripts/launch_all_10_times.sh
+++ scripts/launch_all_10_times.sh
@@ -6,11 +6,9 @@ ROOT=$(cd "$(dirname "$0")/.." && pwd)
 # This is the temporary folder that 'launch_all.sh' uses for it's output files
 # (it MUST CORRISPOND TO "RESULTS_DIR" in 'launch_all.sh')
 CURRENT_RUN_DIR="i_miei_risultati"
-
-
 LAUNCH_SCRIPT="$ROOT/scripts/launch_all.sh"
-
 FINAL_RESULTS_BASE_DIR="$ROOT/results"
+EXPECTED_FILES=80
 mkdir -p "$FINAL_RESULTS_BASE_DIR"
 
 
@@ -37,11 +35,37 @@ for i in $(seq 1 10); do
 
 
     job_count=$(qstat -u $USER | wc -l)
+    file_count=0
+    wait_time_files=0
+    MAX_WAIT_FILE=300
 
-    while [ $job_count -gt 0 ]; do
-        echo "Still waiting for: $job_count jobs to terminate"
-        sleep 60
+    while true; do
         job_count=$(qstat -u $USER | wc -l)
+        file_count=$(find "$CURRENT_RUN_DIR" -name "*.out" | wc -l)
+        
+        
+        if [ "$job_count" -eq 0 ] && [ "$file_count" -ge "$EXPECTED_FILES" ]; then
+            echo "Success!"
+            break
+        fi
+
+        
+        if [ "$job_count" -eq 0 ] && [ "$file_count" -lt "$EXPECTED_FILES" ]; then
+            echo "Waiting for other files..."
+            
+            wait_time_files=$((wait_time_files + 30))
+            
+            # Se aspettiamo troppo (5 minuti a vuoto), forziamo l'uscita
+            if [ "$wait_time_files" -ge "$MAX_WAIT_FILES" ]; then
+                echo "TIMEOUT"
+                break
+            fi
+        else
+            wait_time_files=0
+            echo "In queue: $job_count | File Ready: $file_count/$EXPECTED_FILES"
+        fi
+
+        sleep 30
     done
 
     echo "Batch $i completed!!"
@@ -49,6 +73,10 @@ for i in $(seq 1 10); do
     FINAL_RUN_NAME="run_${i}"
     
     mv "$CURRENT_RUN_DIR" "$FINAL_RESULTS_BASE_DIR/$FINAL_RUN_NAME"
+
+    if [ "$file_count" -lt "$EXPECTED_FILES" ]; then
+        echo "WARNING: There should be more files... Check manually :)"
+    fi
 done
 
 echo "ALL COMPLETE!!"
