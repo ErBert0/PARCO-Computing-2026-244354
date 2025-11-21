10dec250c6bbabfebfd14481bd43fbaf6f5c1d9d
Matteo Bertè <berte.mtt@gmail.com>
2025-11-21 18:30:17 +0100

    messo march=native

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -9,8 +9,8 @@ module load gcc91
 #Here you can modify the Compiler and the Compilation Flags
 COMPILER="g++-9.1.0"
 
-$COMPILER -O3 -o "$ROOT/Sequential.out" "$ROOT/src/Sequential.cpp"
-$COMPILER -O3 -fopenmp -o "$ROOT/Parallel.out" "$ROOT/src/Parallel.cpp"
+$COMPILER -O3 -march=native -o "$ROOT/Sequential.out" "$ROOT/src/Sequential.cpp"
+$COMPILER -O3 -march=native -fopenmp -o "$ROOT/Parallel.out" "$ROOT/src/Parallel.cpp"
 
 echo "Compilation Terminated."
 
